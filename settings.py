0622e42dcf690a99bf24b43c2c7fa04b32eadbdf
Armine Akhverdian <armineakh@ld.yandex.ru>
2018-09-29 17:00:24 +0300

    del file

--- settings.py
+++
@@ -1,4 +0,0 @@
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
-
-API_KEY = "688091655:AAEuXCkQsgklzMqX1tkb1ehA-UDHQkP_cVw"
