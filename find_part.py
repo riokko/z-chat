ad6af0a5b9f06f0752b29ab85807d6ff29f9b069
Daria Primorskaya <riokko@rizn.org>
2018-09-30 17:34:45 +0300

    умеем посылать информацию разными сообщениями

--- find_part.py
+++ find_part.py
@@ -7,14 +7,12 @@ def find_part(bot, update):
     c = Car
     
     user_phrase = update.message.text
-    user_phrase = user_phrase.upper()
-    user_phrase = user_phrase.split(" ")
-    user_phrase = user_phrase[1:]
+    user_phrase = user_phrase.upper().split(' ')[1:]
     user_phrase = ''.join(user_phrase)
 
     for symbol in ruseng_letters:                   # проверяем, если первый символ — русская буква
         if user_phrase[0] == symbol:
-            user_phrase_eng = "" 
+            user_phrase_eng = '' 
             for symbol in user_phrase:
                 user_phrase_eng += ruseng_letters.get(symbol) # заменяем русские буквы на английские
             user_phrase = user_phrase_eng
@@ -24,11 +22,13 @@ def find_part(bot, update):
 
     user_phrase = '%{}%'.format(user_phrase)
 
-    data_upload = c.query.filter(c.licence_plate.like(user_phrase)).all()
+    data_upload = c.query.filter(c.licence_plate.like(user_phrase)).first()
     if data_upload == []:                           # если фильтр из базы — пустой список
-        reply = "Такого номера нет в базе"
-        update.message.reply_text(reply)
+        reply_phrase = 'Такого номера нет в базе'
+        update.message.reply_text(reply_phrase)
+
     else:                                           # если есть совпадения
-        data_upload = str(data_upload)              # переводим в строку
-        data_upload = data_upload[1:-1]             # убираем символы [ и ]
-        update.message.reply_text(data_upload)
+        owner_phone = 'Владелец {}, номер телефона {}'.format(data_upload.car_owner, data_upload.phone_number)
+        photo = '{}'.format(data_upload.photo)
+        update.message.reply_text(photo)
+        update.message.reply_text(owner_phone)    
