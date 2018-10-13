62d09a09bba8591a752e2d38837514f901b1eb35
Armine Akhverdian <armineakh@ld.yandex.ru>
2018-10-13 11:50:01 +0300

    Убрала ненужное

--- bot_main.py
+++ bot_main.py
@@ -7,10 +7,8 @@ import logging
 
 import settings
 import sort_by_numbers
-from keyboard_add import try_keyboard, add_info
 from find_part import find_part
 from edit_func import conv_handler
-#from keyboard_add import get_info
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -26,7 +24,7 @@ def main():
     dp.add_handler(CommandHandler("find", find_part, pass_user_data=True))
     dp.add_handler(conv_handler)
     dp.add_handler(CommandHandler("plates", sort_by_numbers.plates))
-    dp.add_handler(RegexHandler("^(Добавить новую машину)$",add_info, pass_user_data=True))
+    #dp.add_handler(RegexHandler("^(Добавить новую машину)$",add_info, pass_user_data=True))
 
     mybot.start_polling()
     mybot.idle()
